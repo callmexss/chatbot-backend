10edaef953c1825e6db09a0ce44ebeffb3994987
callmexss <callmexss@126.com>
2023-08-29 01:18:17 +0800

    feat: enable streaming.

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import StreamingHttpResponse
 from gptbase import basev2
 
 from .models import Message
@@ -21,15 +22,29 @@ def echo_message(request):
 assistant = basev2.Assistant()
 
 
+def save_and_yield_original(gen):
+    saved = []
+    for item in gen:
+        saved.append(item)
+        yield item
+    message_bot = Message(content=''.join(saved), message_type='bot')
+    message_bot.save()
+
+
 @api_view(['POST'])
 def openai_message(request):
     content: str = request.data.get('content')
     message = Message(content=content, message_type='user')
     message.save()
-    chat_completion = assistant.ask(content)
-    reply = basev2.get_message(chat_completion)
-    message = Message(content=reply, message_type='bot')
-    message.save()
-    serializer = MessageSerializer(message)
-    return Response(serializer.data)
+
+    params = basev2.CompletionParameters(stream=True)
+    chat_completion = assistant.ask(content, params=params)
+    new_gen = save_and_yield_original(basev2.get_chunks(chat_completion))
+
+    response = StreamingHttpResponse(
+        (chunk for chunk in new_gen),
+        content_type="text/plain"
+    )
+
+    return response
 
