f9567538d2237edb95647a790859c3088b4200e3
callmexss <callmexss@126.com>
2023-08-29 21:42:53 +0800

    refactor: reduce repeat code.

--- chat/views.py
+++ chat/views.py
@@ -4,23 +4,15 @@ from django.http import StreamingHttpResponse
 from gptbase import basev2
 
 from .models import Message
-from .serializers import MessageSerializer
-
-
-# Create your views here.
-@api_view(['POST'])
-def echo_message(request):
-    content: str = request.data.get('content')
-    message = Message(content=content, message_type='user')
-    message.save()
-    message = Message(content=content, message_type='bot')
-    message.save()
-    serializer = MessageSerializer(message)
-    return Response(serializer.data)
-
 
 assistant = basev2.Assistant()
 assistant = basev2.ChatAssistant()
+
+
+def save_message(content, message_type):
+    message = Message(content=content, message_type=message_type)
+    message.save()
+    return message
 
 
 def save_and_yield_original(gen):
@@ -29,25 +21,39 @@ def save_and_yield_original(gen):
         saved.append(item)
         yield item
     message = ''.join(saved)
-    message_bot = Message(content=message, message_type='bot')
-    message_bot.save()
+    save_message(message, 'bot')
     assistant.q.append(assistant.build_assistant_message(message))
 
 
-@api_view(['POST'])
-def openai_message(request):
+def handle_post_request(request, reply_function):
     content: str = request.data.get('content')
+    save_message(content, 'user')
+    return reply_function(content)
+
+
+def echo_reply(content):
+    save_message(content, 'bot')
+    return Response({"content": content, "message_type": "bot"})
+
+
+def openai_reply(content):
     assistant.q.append(assistant.build_user_message(content))
-    message = Message(content=content, message_type='user')
-    message.save()
 
     params = basev2.CompletionParameters(stream=True)
     chat_completion = assistant.ask(assistant.q, params=params)
     new_gen = save_and_yield_original(basev2.get_chunks(chat_completion))
 
-    response = StreamingHttpResponse(
+    return StreamingHttpResponse(
         (chunk for chunk in new_gen),
         content_type="text/plain"
     )
 
-    return response
+
+@api_view(['POST'])
+def echo_message(request):
+    return handle_post_request(request, echo_reply)
+
+
+@api_view(['POST'])
+def openai_message(request):
+    return handle_post_request(request, openai_reply)
